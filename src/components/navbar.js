b1a9466ab1524dc3dd28b82cf6b151ced7c938ba
Ryan Kiilu <63748600+ThePapaa@users.noreply.github.com>
2023-02-01 10:18:18 +0000

    lunch

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,22 +1,57 @@
-import { Link } from "react-router-dom"
+import { Link, NavLink } from "react-router-dom";
+import { NavLinks } from "./navlinks";
+import { useState } from "react";
 
 const Navbar = () => {
-    return (
-        <nav className="home-nav-bar">
-            <div className="logo">
-                <Link to="/"><h1>RECETTE</h1></Link>
-            </div>
-            <ul className='flex-row'>
-                <li><a href=""><i className="fa-solid fa-magnifying-glass"></i> FIND RECIPES</a></li>
-                <li><Link to="/tests">TEST SITE</Link></li>
-                <li><Link to='recipes'>POST RECIPE</Link></li>
-                <li><Link to='/'>CREATE ACCOUNT</Link></li>
-            </ul>
-            <div className="menu-bar">
-                <i class="fa-solid fa-bars"></i>
-            </div>
-        </nav>
-    )
-}
+  const [openSideNav, setOpenSideNav] = useState(false);
 
-export default Navbar
+  let linkHeight = 100 / NavLinks.length;
+  return (
+    <nav className="home-nav-bar">
+      <div className="logo">
+        <Link to="/">
+          <h1>RECETTE</h1>
+        </Link>
+      </div>
+      <ul className="flex-row">
+        <li>
+          <Link to="#">
+            <i className="fa-solid fa-magnifying-glass"></i> FIND RECIPES
+          </Link>
+        </li>
+        <li>
+          <Link to="/tests">TEST SITE</Link>
+        </li>
+        <li>
+          <Link to="recipes">POST RECIPE</Link>
+        </li>
+        <li>
+          <Link to="/">CREATE ACCOUNT</Link>
+        </li>
+      </ul>
+      <div className="menu-bar">
+        <button
+          className="menu-btn"
+          onClick={() => {
+            setOpenSideNav(!openSideNav);
+          }}
+        >
+          <i class="fa-solid fa-bars"></i>
+        </button>
+      </div>
+      <div className="sidenav">
+        <div className="links-list">
+          {NavLinks.map((item, index) => {
+            return (
+              <Link to={item.url}>
+                {item.name}
+              </Link>
+            );
+          })}
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
---
+++ src/components/navlinks.js
@@ -0,0 +1,26 @@
+export const NavLinks = [
+  //   {
+  //     name: "HOME",
+  //     to: "/",
+  //     tag: "HOME",
+  //   },
+  {
+    name: "POST RECIPE",
+    url: "/recipes",
+    tag: "RECIPES",
+  },
+  {
+    name: "ALL RECIPES",
+    url: "/all-recipes",
+    tag: "ALL RECIPES",
+  },
+  {
+    name: "TEST SITE",
+    url: "\tests",
+    tag: "Welcome to the test site",
+  },{
+    name:"HELP",
+    url:"",
+    tag:"Get help"
+  }
+];
